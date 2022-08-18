e40bdb93e96d511899023886dd7c31556cba7ce7
Edgar Arturo Martínez <edgar.arturo.martinez@gmail.com>
2022-08-18 16:15:10 -0500

    Commit with Angular configuration and pm Packages added.

--- WebApi_NetCore6Angular/Data/DataContext.cs
+++ WebApi_NetCore6Angular/Data/DataContext.cs
@@ -4,6 +4,12 @@ namespace WebApi_NetCore6Angular.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
+        public DbSet<Inspection> Inspections { get; set; }
+
+        public DbSet<InspectionType> InspectionTypes { get; set; }
+
+        public DbSet<Status> Statuses { get; set; }
     }
 }
